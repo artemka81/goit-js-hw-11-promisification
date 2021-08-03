2f2489afd3adb49a5321d598dfe8da2bc3ce8a5a
Artem Kikhtenko <artemka81@gmail.com>
2021-08-03 23:38:06 +0300

    Task2 - DONE!

--- task1.js
+++ task1.js
@@ -1,3 +1,4 @@
+// ==============  Task 1 ====================
 const delay = (ms) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -13,3 +14,4 @@ const logger = (time) => console.log(`Resolved after ${time}ms`);
 delay(2000).then(logger); // Resolved after 2000ms
 delay(1000).then(logger); // Resolved after 1000ms
 delay(1500).then(logger); // Resolved after 1500ms
+
